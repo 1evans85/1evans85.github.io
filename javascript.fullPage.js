56df8d7d16b5447638b7fd8e3b661a7f07a42d73
Shu <1evans85@solent.ac.uk>
2017-11-13 17:31:22 +0000

    Update javascript.fullPage.js

--- javascript.fullPage.js
+++ javascript.fullPage.js
@@ -103,9 +103,9 @@
         // Create some defaults, extending them with any options that were provided
         var defaults = {
             //navigation
-            menu: false,
+            menu: true,
             anchors:[],
-            navigation: false,
+            navigation: true,
             navigationPosition: 'right',
             navigationColor: '#000',
             navigationTooltips: [],
